c5b6ca5a952fa890354a92d50681103cd8d10d30
laerts <la@eenwereldmetlef.be>
2023-01-11 12:07:41 +0100

    version constraints

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 Package.describe({
-  name: 'ermlab:office365-oauth',
+  name: 'lef:office365-oauth',
   version: '0.2.0',
   summary: 'Microsoft Office 365 OAuth flow',
   git: 'https://github.com/Ermlab/meteor-office365-oauth.git',
@@ -8,7 +8,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.5.1');
+  api.versionsFrom('2.7.1');
 
   api.use('ecmascript');
   api.use('oauth2', ['client', 'server']);
